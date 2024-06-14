9186412a62e0f1801858107ee85afe001953dc8f
DFST <mk@dfst.io>
2024-06-14 16:47:37 +0400

    jwt

---
+++ src/lib/jwt.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import axios from "axios";
+
+export async function generateJWT(address: string): Promise<string> {
+  let answer = await zkCloudWorkerRequest({
+    command: "generateJWT",
+    id: address,
+    mode: "async",
+  });
+
+  console.log(`zkCloudWorker answer:`, answer);
+  return answer;
+}
+
+async function zkCloudWorkerRequest(params: any) {
+  const { command, id, mode } = params;
+  const apiData = {
+    auth: process.env.NEXT_PUBLIC_ZKCW_AUTH,
+    command: command,
+    jwtToken: process.env.NEXT_PUBLIC_ZKCW_JWT,
+    data: {
+      id,
+      mode: mode ?? "sync",
+    },
+    chain: `mainnet`,
+  };
+  const endpoint = process.env.NEXT_PUBLIC_ZKCW_ENDPOINT + "mainnet";
+
+  const response = await axios.post(endpoint, apiData);
+  return response.data;
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
