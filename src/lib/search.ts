0090faaed8c00ce74effa6181a645c26cfa59b6b
DFST <mk@dfst.io>
2024-07-09 15:28:42 +0400

    zk-jobs

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -5,7 +5,7 @@ const client = algoliasearch(
   process.env.ALGOLIA_PROJECT!,
   process.env.ALGOLIA_KEY!
 );
-const index = client.initIndex("jobs");
+const index = client.initIndex("zk-jobs");
 
 export async function searchJobs(params: {
   query: string;
@@ -13,17 +13,23 @@ export async function searchJobs(params: {
   currentPage: number;
 }): Promise<{ hits: any[]; nbHits: number; nbPages: number; page: number }> {
   const { query, hitsPerPage, currentPage } = params;
+  console.log({ query, hitsPerPage, currentPage });
   // TODO: limit return fields
-  const { hits, nbHits, nbPages, page } = await index.search(query, {
+
+  const result = await index.search(query, {
     hitsPerPage,
-    page: currentPage,
+    page: currentPage ?? 0,
     attributesToRetrieve: [
       "jobId",
       "timeCreated",
       "repo",
       "jobStatus",
       "metadata",
+      "chain",
     ],
   });
+
+  console.log(result);
+  const { hits, nbHits, nbPages, page } = result;
   return { hits, nbHits, nbPages, page };
 }
